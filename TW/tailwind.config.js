b5e8f63d8e7af41a11c8de98e4d0989a6df3665b
Drportox <guiportopt@outlook.com>
2025-01-05 23:02:50 +0000

    tailwindCSS done

--- TW/tailwind.config.js
+++ TW/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: 'b7-', //aqui crio um prefixo para eu saber que e do tailwind, porem tera que usar esse profixo sempre q for usar as propriedades do tailwind, nao e tao util.
   content: [
     './p2/**/*.html',//busca por classes tds arquivos html dentro de P2
     './P2/src/**/*.{js,ts,jsx,tsc}'// Busca por classes em arquivos JS/TS na pasta src
